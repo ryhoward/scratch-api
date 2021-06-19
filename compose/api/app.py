0ff80894b900f9645179bf59835e7ee3c9b1f4b2
ryhoward <ryhow1@gmail.com>
2021-06-19 01:45:09 +0000

    saving some work on basic db setup and secrets

--- compose/api/app.py
+++ compose/api/app.py
@@ -1,18 +1,20 @@
 import os
 from flask import jsonify, request, Flask
-from get_docker_secret import get_docker_secret
+#from get_docker_secret import get_docker_secret
 
 import mysql.connector
 
 
 
 class DBManager:
-    def __init__(self, database='users', host="db", user="root"):
-        #pf = open(password_file, 'r')
-        db_secret = get_docker_secret('root-db-pw', default='test-secret')
+    def __init__(self, database='users', host="db", user="root", password_path=None):
+        #pw_path = open(password_file, 'r')
+        #db_secret = get_docker_secret('root-db-pw', default='test-secret')
+        db_secret = os.getenv('root-db-pw') 
         self.connection = mysql.connector.connect(
             user=user, 
-            password=db_secret,
+            #password=pw_path.read(),
+            #password
             host=host, # name of the mysql service as set in the docker-compose file
             database=database,
             auth_plugin='mysql_native_password'
@@ -26,8 +28,8 @@ class DBManager:
         #self.cursor.executemany('INSERT INTO users (id, name) VALUES (%s, %s);', [(i, 'user name #%d'% i) for i in range (1,5)])
         self.connection.commit()
     
-    def get_user(self):
-        self.cursor.execute('SELECT name FROM users WHERE id')
+    def get_user(self, id):
+        self.cursor.execute('SELECT name FROM users WHERE id = %s', id)
         rec = []
         for c in self.cursor:
             rec.append(c[0])
@@ -53,18 +55,25 @@ def users(name):
 
 @app.route('/users',methods = ['GET'])
 def users(id):
-    global conn
-    if not conn:
-        conn = DBManager()
-        conn.get_user()
-    user = request.args.get('nm')
-     return 'get user'
+    #global conn
+    #if not conn:
+    #    conn = DBManager()
+    conn = db_connection()
+    user = conn.get_user()
+   # user = request.args.get('nm')
+    return user
 
 
 @app.route('/users/<transaction_id>')
 def users_id():
     return 'get user by id {}'.format(transaction_id)
 
+
+def db_connection():
+    global conn
+    if not conn:
+        conn = DBManager(password_path='/run/secrets/root-db-pw')
+        return conn
 
 #@app.route('/')
 #def hello_world():
@@ -73,4 +82,4 @@ def users_id():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
+    
